f54aa692f30e39eaf03fa059c653d66e3bfa0a88
Fahim-Foysal-Apurba <fahim.apurba@northsouth.edu>
2025-03-06 17:34:32 +0600

    lll

--- Backend/index.js
+++ Backend/index.js
@@ -97,14 +97,6 @@ app.post('/createSalesforceAccount', async (req, res) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
-
-
-
-
-
 
 
 // Register User
